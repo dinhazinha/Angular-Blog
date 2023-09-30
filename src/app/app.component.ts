95469daeb41b04f7716bd3dfce57a015ddad598c
Ildecilda Fatima dos Santos <109155497+dinhazinha@users.noreply.github.com>
2023-09-30 18:52:08 -0300

    alterações necessárias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`
+  <h1>Aula de services</h1>
+  <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
