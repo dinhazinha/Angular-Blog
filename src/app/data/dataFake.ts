107557246e3aeda212413d96450f9ec2cbe39b0b
Ildecilda Fatima dos Santos <109155497+dinhazinha@users.noreply.github.com>
2023-10-05 22:30:43 -0300

    alterações

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
 {
-  "id":1,
-  "photo":"Dinha",
-  "title": "Estudando Angular",
-  "description":"maravilhada como é mágico"
+  "id":"1",
+  "photoCover":"https://miro.medium.com/v2/resize:fit:1400/1*bMgQ8MhbnQexpqHgIgBJPA.png",
+  "title": "Dimensionando aplicativos Angular",
+  "description":"Angular funciona com aplicativos de qualquer tamanho e as ferramentas Angular oferecem suporte a aplicativos à medida que crescem."
 },
 {
-  "id":2,
-  "photo":"Dinha2",
-  "title": "Estudando Angular2",
-  "description":"maravilhada como é mágico top demais"
+  "id":"2",
+  "photoCover":"https://www.etatvasoft.com/blog/wp-content/uploads/2017/01/b-thumb-img12.jpg",
+  "title": "Conheça o novo fluxo de controle do Angular",
+  "description":"O futuro traz uma nova sintaxe para fluxo de controle e carregamento adiável"
+},
+{
+  "id":"3",
+  "photoCover":"https://devtop.io/wp-content/uploads/2023/01/angular-js.jpg",
+  "title": "Antes de aprender sobre AngularJS",
+  "description":"Antes de tudo, você precisa lembrar que o AnglarJS é um framework JavaScript."
+},
+{
+  "id":"4",
+  "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYY0MTE7f68Cj_bbc0MMleCJI9XXfc754QhAqP_4kaKTYhm7BduFc7pvhFLWvcBrNz65A&usqp=CAU",
+  "title": "As Vantagens do AngularJS",
+  "description":"Existem alguns recursos do Angular que suportam o dimensionamento do seu aplicativo Angular."
 }
 ]
